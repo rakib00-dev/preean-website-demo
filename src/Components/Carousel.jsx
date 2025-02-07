5cfbd354d99d360501d2f0948fb6177032b36685
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-08 03:28:27 +0600

    just added the caurosel but not finished yet

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,11 +1,23 @@
 import CountUp from 'react-countup';
 
 const Carousel = () => {
+  const images = [];
+  images[1] = '../images/carousel/alone-bird.svg';
+  images[2] = '../images/carousel/buffer.svg';
+  images[3] = '../images/carousel/carousel.svg';
+  images[4] = '../images/carousel/decathlon.svg';
+  images[5] = '../images/carousel/dynamite-jobs.svg';
+  images[6] = '../images/carousel/eiv.svg';
+  images[7] = '../images/carousel/sleek.svg';
+  images[8] = '../images/carousel/tech-able.svg';
+
   return (
-    <section className={`m-auto w-full mt-[4rem] items-center bg-white `}>
+    <section
+      className={`m-auto w-full mt-[4rem] items-center bg-white overflow-hidden`}
+    >
       <div className="flex m-auto justify-center items-center flex-col w-[95%] gap-8 md:flex-row">
         {/* counter */}
-        <div className="holi">
+        <div className="">
           {/* counter 1 */}
           <div className="flex pt-10 flex-col justify-center items-center">
             <div
@@ -57,7 +69,44 @@ const Carousel = () => {
           </div>
         </div>
         {/* carousel */}
-        <div className=""></div>
+        <div className="flex gap-4 overflow-hidden ">
+          <div className="inline-block slider">
+            {/* slide 1 */}
+            <div className="inline-block slider">
+              <div className="slide">
+                {images.map((e, i) => (
+                  <span key={i} className="">
+                    <img src={e} alt={e} className="w-[7rem] inline" />
+                  </span>
+                ))}
+              </div>
+              <div className="slide">
+                {images.map((e, i) => (
+                  <span key={i} className="">
+                    <img src={e} alt={e} className="w-[7rem] inline" />
+                  </span>
+                ))}
+              </div>
+            </div>
+            {/* slide 2 */}
+            <div className="slider">
+              <div className="slide2">
+                {images.map((e, i) => (
+                  <span key={i} className="">
+                    <img src={e} alt={e} className="w-[7rem] inline" />
+                  </span>
+                ))}
+              </div>
+              <div className="slide2">
+                {images.map((e, i) => (
+                  <span key={i} className="">
+                    <img src={e} alt={e} className="w-[7rem] inline" />
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
