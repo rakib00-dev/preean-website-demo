cbd0c63a006c6bec279dde1270070046880f816b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-08 16:19:34 +0600

    updated the layout

--- src/Components/FirstTypeSection.jsx
+++ src/Components/FirstTypeSection.jsx
@@ -5,7 +5,6 @@ const FirstTypeSection = ({
   imgSrc,
   style,
   contentClass,
-  textContentAlign = 'start',
 }) => {
   return (
     <section className="flex flex-col justify-center items-center m-auto w-full my-[4rem] bg-white overflow-hidden md:w-4/5">
@@ -22,23 +21,22 @@ const FirstTypeSection = ({
         >
           {/* text conent */}
           <div
-            className={`flex flex-col justify-center items-center lg:items-${textContentAlign} lg:gap-2`}
+            className={`flex flex-col justify-center items-center lg:items-start lg:gap-2`}
           >
             <h1
-              className={`hidden w-full uppercase font-bold text-[.85rem] md:block lg:text-[1rem] lg:text-${textContentAlign}`}
+              className={`hidden w-full uppercase font-bold text-[.85rem] md:block lg:text-[1rem] lg:text-start`}
               style={{ color: 'var(--colors-pink)' }}
             >
               {smallTitle}
             </h1>
             <h2
-              className={`font-extrabold text-[1.4rem] text-center w-4/5 mb-4 leading-8 lg:text-${textContentAlign} min-[450px]:w-full md:text-[2.1rem] lg:w-[95%] lg:text-[2.5rem] lg:leading-13`}
+              className={`font-extrabold text-[1.4rem] text-center w-4/5 mb-4 leading-8 lg:text-start min-[450px]:w-full md:text-[2.1rem] lg:w-[95%] lg:text-[2.5rem] lg:leading-13`}
               style={style}
             >
               {bigTitle}
             </h2>
             <p
-              className={`text-center text-sm px-13 w-[24.6rem] md:w-[26rem] md:px-0 md:text-lg lg:text-${textContentAlign}  lg:text-2xl`}
-              style={style}
+              className={`text-center text-sm px-13 w-[24.6rem] md:w-[26rem] md:px-0 md:text-lg lg:text-start  lg:text-2xl`}
             >
               {para}
             </p>
@@ -50,7 +48,6 @@ const FirstTypeSection = ({
               src={imgSrc}
               alt="need-designer-soon"
               className="" /*{hue-rotate-353} */
-              loading="lazy"
             />
           </div>
         </div>
