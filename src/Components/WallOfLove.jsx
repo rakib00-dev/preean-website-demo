795eff3653881e780a50d8cbef91aa44fa012b69
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-09 12:36:44 +0600

    removed the swiper js

--- src/Components/WallOfLove.jsx
+++ src/Components/WallOfLove.jsx
@@ -7,14 +7,12 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import '../index.css';
-
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
 function WallOfLove() {
   return (
-    <section className="flex flex-col justify-center items-center mx-auto w-full max-w-7xl md:mt-16 md:pt-10 bg-white overflow-hidden ">
+    <section className="flex flex-col justify-center items-center mx-auto w-full max-w-7xl md:mt-16 md:pt-10 bg-white  ">
       <div className="flex m-auto justify-center items-center flex-col w-full gap-4 ">
         <div
           className={`flex flex-col justify-center gap-2 items-center lg:gap-4`}
@@ -51,41 +49,19 @@ function WallOfLove() {
             modules={[Pagination, Navigation]}
             className="mySwiper"
           >
-            <SwiperSlide>
-              {' '}
-              <div className="flex justify-center items-center w-[90%] flex-col bg-gray-100 p-4 py-6 text-center mb-5 rounded-lg gap-2 md:flex-row swiper-slide">
-                <img
-                  src="	https://cdn.prod.website-files.com/63a9cb71c629474…ae334b9/64771ca81ca155824e1c4c08_Group%204818.svg"
-                  alt=""
-                  loading="lazy"
-                  className="w-24"
-                />
-
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-xl font-extrabold ">Fast & Reliable</h3>
-                  <p className="text-md ">
-                    Get your design back in 1-2 business days.
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              {' '}
-              <div className="flex justify-center items-center w-[90%] flex-col bg-gray-100 p-4 py-6 text-center mb-5 rounded-lg gap-2 md:flex-row swiper-slide">
-                <img
-                  // src="	https://cdn.prod.website-files.com/63a9cb71c629474…ae334b9/64771ca81ca155824e1c4c08_Group%204818.svg"
-                  alt=""
-                  loading="lazy"
-                  className="w-24"
-                />
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-xl font-extrabold ">Fast & Reliable</h3>
-                  <p className="text-md ">
-                    Get your design back in 1-2 business days.
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">hello </SwiperSlide>
+            <SwiperSlide className="swiper-slide">hwllo </SwiperSlide>
+          </Swiper>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
           </Swiper>
         </div>
       </div>
