0a47b7022dc26f94f00588839d2dcb589cf661ae
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-09 15:50:44 +0600

    added the clienreview mobile view

--- src/Components/ClientReview.jsx
+++ src/Components/ClientReview.jsx
@@ -5,21 +5,21 @@ const ClientReview = () => {
       thought: `“If my bestfriend was on the fence about trying ManyPixels, I would tell them that they needed to do it, because it’s a no-brainer.”`,
       name: 'Jeanette Knutti',
       position: 'CEO, Moxie Marketing',
-      group: './images/clientreview/client1group.avif',
+      group: './images/clientreview/client1group.svg',
     },
     {
       img: './images/clientreview/client2.avif',
       thought: `“I love the quality of communication, the ease of platform. You can literally get out of bed, open your laptop and get critical things accomplished, without a lot back-and-forth or over-communicating.”`,
       name: 'Sean Mackenzie',
       position: 'Team Lead, Citi Habitats',
-      group: './images/clientreview/client2group.avif',
+      group: './images/clientreview/client2group.svg',
     },
     {
       img: './images/clientreview/client3.avif',
       thought: `“The potential upside is so big. Your stuff could look good, it could land you bigger clients, and you could just have your entire brand be better. It’s a very low risk - high reward, so go ahead and just do it.”`,
       name: 'Itamar Marani',
       position: 'Founder & CEO, Marani Consulting',
-      group: './images/clientreview/client3group.avif',
+      group: './images/clientreview/client3group.svg',
     },
   ];
 
@@ -31,7 +31,7 @@ const ClientReview = () => {
           <select
             name="type"
             id="type"
-            className="bg-gray-100 py-4 px-8 rounded-sm"
+            className="bg-gray-100 text-md py-4 px-8 rounded-md"
           >
             <option value="agencies">Agencies</option>
             <option value="marketing teams">Marketing teams</option>
@@ -44,13 +44,20 @@ const ClientReview = () => {
           <div>entrepreneurs</div>
         </div>
         {client.slice(0, 1).map((e, i) => (
-          <div className="" key={i}>
+          <div className="grid gap-4" key={i}>
             {/* client img */}
-            <div className="w-full">
-              <img src={e.img} alt={e.thought} />
+            <div className=" w-full">
+              <img src={e.img} alt={e.thought} loading="lazy" />
             </div>
             {/* about him/her */}
-            <div className=""></div>
+            <article className="grid gap-3">
+              <p>{e.thought}</p>
+              <div className="leading-10">
+                <h5 className="text-xl font-extrabold">{e.name}</h5>
+                <p>{e.position}</p>
+                <img src={e.group} alt={e.position} loading="lazy" />
+              </div>
+            </article>
           </div>
         ))}
       </div>
