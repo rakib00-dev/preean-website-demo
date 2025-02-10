f483d23adc52f867bece1f2e7e4507fae66f92ff
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-10 09:33:35 +0600

    updated the our work page and added the page functionnality

--- src/Components/PageHeader.jsx
+++ src/Components/PageHeader.jsx
@@ -1,7 +1,7 @@
 const PageHeader = () => {
   return (
-    <section>
-      <div className="">
+    <section className="mx-auto w-full max-w-7xl ">
+      <div className="relative">
         <div
           className={`flex flex-col justify-center gap-2 items-center lg:gap-4`}
         >
@@ -10,12 +10,44 @@ const PageHeader = () => {
           >
             Our Work
           </h2>
-          <p
-            className={`text-center text-md px-13 md:w-[26rem] md:px-0 md:text-lg `}
-          >
-            Don’t take our word for it - see what some of our happy customers
-            think about the service.
+          <p className={`text-center text-xl px-2 w-full md:px-0 md:text-2xl `}>
+            Check out some of the latest creations we did for our customers
           </p>
+          <div className="flex absolute px-5 top-0 items-center justify-between -z-10 w-full">
+            <img
+              src="./images/ourworkpage/yellow-squre.svg"
+              alt="this is a yello element which is use to inchnge the design quality"
+              loading="lazy"
+              className="w-10 left-[10%] absolute -top-5 md:w-24"
+            />
+            <div className="absolue left-[25%] grid w-full">
+              <img
+                src="./images/ourworkpage/ring.svg"
+                alt="this is a ring element which is use to inchnge the design quality"
+                loading="lazy"
+                className="hidden top-30 absolute  left-[20%] md:block md:top-35 md:left-[25%]"
+              />
+              <img
+                src="./images/ourworkpage/blue-squre.svg"
+                alt="this is a blue squre element which is use to inchnge the design quality"
+                loading="lazy"
+                className="hidden bottom-5 left-[35%] absolute md:block"
+              />
+            </div>
+
+            <img
+              src="./images/ourworkpage/tringle.svg"
+              alt="this is a tringle element which is use to inchnge the design quality"
+              loading="lazy"
+              className="w-10 absolute top-5 right-[10%] md:right-[25%] md:w-8"
+            />
+            <img
+              src="./images/ourworkpage/double-squre.svg"
+              alt="this is a tringle element which is use to inchnge the design quality"
+              loading="lazy"
+              className="hidden absolute -bottom-30 right-[10%] md:block"
+            />
+          </div>
         </div>
       </div>
     </section>
