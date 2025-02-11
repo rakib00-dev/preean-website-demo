66d90175df55dce00c7e0a4b6e7c738f5b6c8fbb
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-11 15:09:27 +0600

    fixed the mobile view all plans section

--- src/Components/PricingPage/AllPlans.jsx
+++ src/Components/PricingPage/AllPlans.jsx
@@ -4,23 +4,23 @@ const AllPlans = () => {
       <div className="bg-gray-100 grid place-items-center rounded-2xl p-5 py-7 gap-10 mx-2 md:mx-20 ">
         <h1 className="text-3xl font-[900] md:text-4xl">All plans include</h1>
         <ul className="grid md:grid-cols-3 gap-5">
-          <li className="flex gap-2 text-xl md:mx-15">
+          <li className="flex gap-2 md:text-xl md:mx-15">
             <img src="./images/ourpricingpage/card-images/done.svg" alt="" />{' '}
             Unlimited Requests
           </li>
-          <li className="flex gap-2 text-xl md:mx-15">
+          <li className="flex gap-2 md:text-xl md:mx-15">
             <img src="./images/ourpricingpage/card-images/done.svg" alt="" />{' '}
             Unlimited Revisions
           </li>
-          <li className="flex gap-2 text-xl md:mx-15">
+          <li className="flex gap-2 md:text-xl md:mx-15">
             <img src="./images/ourpricingpage/card-images/done.svg" alt="" />{' '}
             Native Source Files
           </li>
-          <li className="flex gap-2 text-xl md:mx-15">
+          <li className="flex gap-2 md:text-xl md:mx-15">
             <img src="./images/ourpricingpage/card-images/done.svg" alt="" />{' '}
             Unlimited Brands
           </li>
-          <li className="flex gap-2 text-xl md:mx-15">
+          <li className="flex gap-2 md:text-xl md:mx-15">
             <img src="./images/ourpricingpage/card-images/done.svg" alt="" />{' '}
             Free Stock Assets
           </li>
