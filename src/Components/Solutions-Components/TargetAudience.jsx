42f8aadc199d06f5575d4f0d07e07ce5d8fbe3ab
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-12 01:41:06 +0600

    changed the target img

--- src/Components/Solutions-Components/TargetAudience.jsx
+++ src/Components/Solutions-Components/TargetAudience.jsx
@@ -9,7 +9,7 @@ const TargetAudience = () => {
         para={
           'Attract, engage, and convert potential clients with a professionally designed graphics and videos for all your social media platforms.'
         }
-        imgSrc={'./images/solutionPage/social-media/agencies-worldwide.webp'}
+        imgSrc={'./images/solutionPage/social-media/reach-your-target.webp'}
         textContentAlign="end"
         style={{ position: 'relative' }}
         contentClass={'lg:flex-row-reverse'}
