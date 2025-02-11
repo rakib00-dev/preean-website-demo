80234ab4627861e01706312e15decfbf3bdfd042
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-12 01:34:21 +0600

    updated the our solu work and targetAudience

---
+++ src/Components/Solutions-Components/TargetAudience.jsx
@@ -0,0 +1,21 @@
+import FirstTypeSection from '../FirstTypeSection';
+
+const TargetAudience = () => {
+  return (
+    <section className="mt-[7rem]">
+      <FirstTypeSection
+        smallTitle={'REACH YOUR TaRGET AUDIENCE'}
+        bigTitle={'Maximize your social media channels'}
+        para={
+          'Attract, engage, and convert potential clients with a professionally designed graphics and videos for all your social media platforms.'
+        }
+        imgSrc={'./images/FirstTypeImg/better-solution.avif'}
+        textContentAlign="end"
+        style={{ position: 'relative' }}
+        contentClass={'lg:flex-row-reverse'}
+      />
+    </section>
+  );
+};
+
+export default TargetAudience;
--- src/pages/SocialMediaPage.jsx
+++ src/pages/SocialMediaPage.jsx
@@ -1,117 +1,21 @@
-import { Link } from 'react-router-dom';
-import Button from '../Components/Button';
 import PricingFaq from '../Components/PricingPage/PricingFaq';
 import SendReqSection from '../Components/SendReqSection';
-import SolutionHeader from '../Components/Solutions Components/SolutionHeader';
+import SolutionHeader from '../Components/Solutions-Components/SolutionHeader';
+import React, { Suspense } from 'react';
+
+const OurSoluWork = React.lazy(() =>
+  import('../Components/Solutions-Components/OurSoluWork')
+);
 
 const SocialMediaPage = () => {
-  const socialMediaImg = [
-    {
-      src: './images/solutionPage/social-media-work/Flex.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-    {
-      src: './images/solutionPage/social-media-work/Health.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-    {
-      src: './images/solutionPage/social-media-work/Glow.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-    {
-      src: './images/solutionPage/social-media-work/Realesatate.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-    {
-      src: './images/solutionPage/social-media-work/Health2.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-
-    {
-      src: './images/solutionPage/social-media-work/Master.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-
-    {
-      src: './images/solutionPage/social-media-work/Nutrition.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-
-    {
-      src: './images/solutionPage/social-media-work/Open.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-
-    {
-      src: './images/solutionPage/social-media-work/Perfect.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-
-    {
-      src: './images/solutionPage/social-media-work/Pit.png',
-      alt: 'this is a social media work of design pro agency and this photo is done by design pro',
-    },
-  ];
-
-  const OurSoluWork = ({
-    smallTitle = 'our work',
-    bigTitle = 'Our latest social media designs',
-  }) => {
-    return (
-      <section className="flex flex-col justify-center items-center mx-auto w-full max-w-7xl md:mt-16 md:pt-10 bg-white overflow-hidden ">
-        <div className="flex m-auto justify-center items-center flex-col w-[95%] gap-4 ">
-          {/* content */}
-
-          {/* text conent */}
-          <div
-            className={`flex flex-col justify-center gap-2 items-center lg:gap-4`}
-          >
-            <h1
-              className="uppercase font-bold text-[.85rem]"
-              style={{ color: 'var(--colors-pink)' }}
-            >
-              {smallTitle}
-            </h1>
-            <h2
-              className={`font-extrabold text-[1.55rem] text-center w-[95%] mb-4 leading-8 lg:text-center min-[450px]:w-full md:text-[2.1rem] lg:w-full lg:text-[2.5rem] lg:leading-13`}
-            >
-              {bigTitle}
-            </h2>
-          </div>
-        </div>
-        <div className="grid place-items-center grid-cols-1 md:grid-cols-5 gap-5 w-4/5 mx-45 my-8 md:my-12">
-          {socialMediaImg.map((e, i) => (
-            <div key={i} className="grid">
-              <img
-                src={e.src}
-                alt={e.alt}
-                loading="lazy"
-                className="rounded-xl"
-              />
-            </div>
-          ))}
-        </div>
-        <div className="flex justify-center items-center mx-auto my-8 w-full md:my-12">
-          <Link to={'/our-work'}>
-            <Button
-              text={'All Our Works'}
-              className={
-                'transition-all border-blue-500 bg-transparent text-blue-500 border-2 hover:bg-blue-500 hover:border-black hover:text-black'
-              }
-              bg="transparent"
-            />{' '}
-          </Link>
-        </div>
-      </section>
-    );
-  };
-
   return (
     <section className="mx-auto w-full max-w-7xl">
       <SolutionHeader />
       {/* our work */}
-      <OurSoluWork />
-
+      <Suspense fallback={<p>Loading...</p>}>
+        <OurSoluWork />
+      </Suspense>
+      <TargetAudience />
       <PricingFaq
         firstTile="What's the turnaround time?"
         p1="Turnaround time depends on various factors, including the type and complexity of the task, the quality of the design brief and the number of revisions needed. On average, you can expect to receive your design the next business day with our Advanced and Business plans and on the same day with our Dedicated Designer plan."
