3e8c743fad7f79579f0d2b51b0a58a9e1f465d13
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-12 07:56:35 +0600

    updated the PostOnSocialMedia section conent and finished the work

--- src/Components/Solutions-Components/PostOnSocialMedia.jsx
+++ src/Components/Solutions-Components/PostOnSocialMedia.jsx
@@ -2,22 +2,221 @@ import SecondTypeSection from '../SecondTypeSection';
 
 const PostOnSocialMedia = () => {
   return (
-    <SecondTypeSection>
-      <div className=" justify-center items-center w-11/12 mx-auto md:flex gap-5">
-        <div className="h-[204px] flex justify-center gap-5 items-center w-full flex-col bg-gray-100 p-4 py-6 text-center mb-5 rounded-lg">
-          <h5 className="font-extrabold text-lg">STRESS LESS</h5>
-          <h3 className="text-4xl font-[900] text-blue-400">4.8</h3>
-          <p>Our average satisfaction rating from hundreds of happy clients</p>
+    <SecondTypeSection
+      smallTitle="POST ON ALL SOCIAL MEDIA CHANNELS"
+      bigTitle="Social media graphics for all platforms"
+    >
+      <div className=" grid grid-cols-1 place-items-center relative w-11/12  md:grid-cols-3">
+        {/* cards */}
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg md:mb-15">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/facebook-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/facebook.svg"
+              alt=""
+              loading="lazy"
+            />
+            Facebook
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Get the most out of your Facebook audience with engaging Images,
+              Videos and Stories.
+            </p>
+          </div>
         </div>
-        <div className="h-[204px] flex justify-center gap-5 items-center w-full flex-col bg-gray-100 p-4 py-6 text-center mb-5 rounded-lg">
-          <h5 className="font-extrabold text-lg">SHIP FASTER</h5>
-          <h3 className="text-4xl font-[900] text-blue-400">24h</h3>
-          <p>Average time to get your first draft back</p>
+        <div className="grid gap-4 relative text-center mb-5 rounded-lg md:mb-20">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/instagram-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/instagram.svg"
+              alt=""
+              loading="lazy"
+            />
+            Instagram
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Up your Instagram game with awesome Posts, Stories, Reels and Ads.
+            </p>
+          </div>
         </div>
-        <div className="h-[204px] flex justify-center gap-5 items-center w-full flex-col bg-gray-100 p-4 py-6 text-center mb-5 rounded-lg">
-          <h5 className="font-extrabold text-lg">Cut Costs</h5>
-          <h3 className="text-4xl font-[900] text-blue-400">50%</h3>
-          <p>Reduced average cost vs alternatives</p>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg md:mb-15">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/google-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/google.svg"
+              alt=""
+              loading="lazy"
+            />
+            Google
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Wether it’s Google Display ads, Videos Ads or Shopping Ads, we got
+              your covered.
+            </p>
+          </div>
+        </div>
+        <div className="grid gap-4 relative text-center mb-5 rounded-lg md:mb-15">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/linkedin-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/linkedin.svg"
+              alt=""
+              loading="lazy"
+            />
+            Linkedin
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Unleash the best B2B network with great captivating graphics.
+            </p>
+          </div>
+        </div>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg md:mb-15">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/pinterest-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/pinterest.svg"
+              alt=""
+              loading="lazy"
+            />
+            Pinterest
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Get more views and get Pin’ned with bespoke graphics.
+            </p>
+          </div>
+        </div>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg md:mb-15">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/youtube-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/youtube.svg"
+              alt=""
+              loading="lazy"
+            />
+            Youtube
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Improve click rate with original thumbnails and motion graphics.
+            </p>
+          </div>
+        </div>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/reddit-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/reddit.svg"
+              alt=""
+              loading="lazy"
+            />
+            Reddit
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Create sunning ads tailored for the biggest online forum.
+            </p>
+          </div>
+        </div>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/twitter-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/twitter.svg"
+              alt=""
+              loading="lazy"
+            />
+            Twitter
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Spice up your tweets and get seen with awesome graphics and
+              videos.
+            </p>
+          </div>
+        </div>
+        <div className=" grid gap-4 relative text-center mb-5 rounded-lg">
+          <div className="md:w-[320px]">
+            <img
+              src="../images/solutionPage/social-media/social-media-post-img/tiktok-post.webp"
+              alt=""
+              loading="lazy"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="font-extrabold flex gap-4 capitalize">
+            <img
+              src="../images/solutionPage/social-media/social-media-icons/tiktok.svg"
+              alt=""
+              loading="lazy"
+            />
+            Tiktok
+          </div>
+          <div className="w-[300px]">
+            <p className="text-start">
+              Jump on the TikTok train and create original videos.
+            </p>
+          </div>
         </div>
       </div>
     </SecondTypeSection>
