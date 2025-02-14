b14683c81a549604a1948ee34d6a1f3c60c5be18
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-14 23:53:09 +0600

    updated the auto change content

--- src/Components/AutoChangeContent.jsx
+++ src/Components/AutoChangeContent.jsx
@@ -1,6 +1,301 @@
 import React, { useEffect, useState } from 'react';
 
 const AutoChangeContent = () => {
+  const imagesArr = [
+    { src: './images/autochange/add-your-availability.png' },
+    { src: './images/autochange/connect-conferencing-tools.png' },
+    { src: './images/autochange/connect-your-calendars.png' },
+    { src: './images/autochange/customize-event-types.png' },
+    { src: './images/autochange/share-scheduling-link.png' },
+  ];
+
+  const [images, setImages] = useState(imagesArr.slice(0, 1));
+
+  const [isFirstActive, setIsFirstActive] = useState(true);
+  const [isSecondActive, setIsSecondActive] = useState(false);
+  const [isThirdActive, setIsThirdActive] = useState(false);
+  const [isFourthActive, setIsFourthActive] = useState(false);
+  const [isFifthActive, setIsFifthActive] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true; // Flag to track if the component is mounted
+
+    const intervalId = setInterval(() => {
+      if (!isMounted) return; // Skip state updates if component is unmounted
+
+      setTimeout(() => {
+        if (isMounted) {
+          setIsFirstActive(true);
+          setIsSecondActive(false);
+          setIsThirdActive(false);
+          setIsFourthActive(false);
+          setIsFifthActive(false);
+          setImages(imagesArr.slice(0, 1));
+        }
+      }, 3500);
+
+      setTimeout(() => {
+        if (isMounted) {
+          setIsFirstActive(false);
+          setIsSecondActive(true);
+          setIsThirdActive(false);
+          setIsFourthActive(false);
+          setIsFifthActive(false);
+          setImages(imagesArr.slice(1, 2));
+        }
+      }, 7000);
+
+      setTimeout(() => {
+        if (isMounted) {
+          setIsFirstActive(false);
+          setIsSecondActive(false);
+          setIsThirdActive(true);
+          setIsFourthActive(false);
+          setIsFifthActive(false);
+          setImages(imagesArr.slice(2, 3));
+        }
+      }, 10500);
+
+      setTimeout(() => {
+        if (isMounted) {
+          setIsFirstActive(false);
+          setIsSecondActive(false);
+          setIsThirdActive(false);
+          setIsFourthActive(true);
+          setIsFifthActive(false);
+          setImages(imagesArr.slice(3, 4));
+        }
+      }, 14000);
+
+      setTimeout(() => {
+        if (isMounted) {
+          setIsFirstActive(false);
+          setIsSecondActive(false);
+          setIsThirdActive(false);
+          setIsFourthActive(false);
+          setIsFifthActive(true);
+          setImages(imagesArr.slice(4, 5));
+        }
+      }, 17500);
+    }, 17500);
+
+    // Cleanup the interval when the component unmounts
+    return () => {
+      isMounted = false;
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const ChangingCards = () => {
+    return (
+      <>
+        <div className="w-full top-0">
+          <div
+            className={
+              isFirstActive
+                ? `transition-all h-auto border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+                : `transition-all h-20 border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+            }
+            onClick={() => {
+              setIsFirstActive(true);
+              setIsSecondActive(false);
+              setIsThirdActive(false);
+              setIsFourthActive(false);
+              setIsFifthActive(false);
+              setImages(imagesArr.slice(0, 1));
+            }}
+          >
+            <h2
+              className={
+                isFirstActive
+                  ? `flex justify-start items-center gap-4 md:text-4xl font-bold text-gray-800`
+                  : `flex justify-start items-center gap-4 md:text-2xl font-bold text-gray-400`
+              }
+            >
+              <img
+                src="./images/autochange/cube.png"
+                alt=""
+                loading="lazy"
+                className={
+                  isFirstActive
+                    ? `w-12  saturate-100`
+                    : `w-8 -brightness-50 saturate-0`
+                }
+              />
+              Connect your calendars
+            </h2>
+            <p className="w-4/5">
+              Calendly connects up to six calendars to automate scheduling with
+              real-time availability.
+            </p>
+          </div>
+          <div
+            className={
+              isSecondActive
+                ? `transition-all h-auto border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+                : `transition-all h-20 border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+            }
+            onClick={() => {
+              setIsFirstActive(false);
+              setIsSecondActive(true);
+              setIsThirdActive(false);
+              setIsFourthActive(false);
+              setIsFifthActive(false);
+              setImages(imagesArr.slice(1, 2));
+            }}
+          >
+            <h2
+              className={
+                isSecondActive
+                  ? `flex justify-start items-center gap-4 md:text-4xl font-bold text-gray-800`
+                  : `flex justify-start items-center gap-4 md:text-2xl font-bold text-gray-400`
+              }
+            >
+              <img
+                src="./images/autochange/trees.png"
+                alt=""
+                loading="lazy"
+                className={
+                  isSecondActive
+                    ? `w-12  saturate-100`
+                    : `w-8 -brightness-50 saturate-0`
+                }
+              />
+              Add your availability
+            </h2>
+            <p className="w-4/5">
+              Keep invitees informed of your availability. Take control of your
+              calendar with detailed availability settings, scheduling rules,
+              buffers, and more.
+            </p>
+          </div>
+          <div
+            className={
+              isThirdActive
+                ? `transition-all h-auto border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+                : `transition-all h-20 border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+            }
+            onClick={() => {
+              setIsFirstActive(false);
+              setIsSecondActive(false);
+              setIsThirdActive(true);
+              setIsFourthActive(false);
+              setIsFifthActive(false);
+              setImages(imagesArr.slice(2, 3));
+            }}
+          >
+            <h2
+              className={
+                isThirdActive
+                  ? `flex justify-start items-center gap-4 md:text-4xl font-bold text-gray-800`
+                  : `flex justify-start items-center gap-4 md:text-2xl font-bold text-gray-400`
+              }
+            >
+              <img
+                src="./images/autochange/triangle.png"
+                alt=""
+                loading="lazy"
+                className={
+                  isThirdActive
+                    ? `w-12  saturate-100`
+                    : `w-8 -brightness-50 saturate-0`
+                }
+              />
+              Connect conferencing tools
+            </h2>
+            <p className="w-4/5">
+              Sync your video conferencing tools and set preferences for
+              in-person meetings or calls.
+            </p>
+          </div>
+          <div
+            className={
+              isFourthActive
+                ? `transition-all h-auto border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+                : `transition-all h-20 border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+            }
+            onClick={() => {
+              setIsFirstActive(false);
+              setIsSecondActive(false);
+              setIsThirdActive(false);
+              setIsFourthActive(true);
+              setIsFifthActive(false);
+              setImages(imagesArr.slice(3, 4));
+            }}
+          >
+            <h2
+              className={
+                isFourthActive
+                  ? `flex justify-start items-center gap-4 md:text-4xl font-bold text-gray-800`
+                  : `flex justify-start items-center gap-4 md:text-2xl font-bold text-gray-400`
+              }
+            >
+              <img
+                src="./images/autochange/cube.png"
+                alt=""
+                loading="lazy"
+                className={
+                  isFourthActive
+                    ? `w-12  saturate-100`
+                    : `w-8 -brightness-50 saturate-0`
+                }
+              />
+              Customize your event type
+            </h2>
+            <p className="w-4/5">
+              Choose from pre-built templates or quickly create custom event
+              types for any meeting you need to schedule.
+            </p>
+          </div>
+          <div
+            className={
+              isFifthActive
+                ? `transition-all h-auto border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+                : `transition-all h-20 border-b py-6 grid gap-5 duration-300 w-11/12 cursor-pointer overflow-hidden`
+            }
+            onClick={() => {
+              setIsFirstActive(false);
+              setIsSecondActive(false);
+              setIsThirdActive(false);
+              setIsFourthActive(false);
+              setIsFifthActive(true);
+              setImages(imagesArr.slice(4, 5));
+            }}
+          >
+            <h2
+              className={
+                isFifthActive
+                  ? `flex justify-start items-center gap-4 md:text-4xl font-bold text-gray-800`
+                  : `flex justify-start items-center gap-4 md:text-2xl font-bold text-gray-400`
+              }
+            >
+              <img
+                src="./images/autochange/cube.png"
+                alt=""
+                loading="lazy"
+                className={
+                  isFifthActive
+                    ? `w-12  saturate-100`
+                    : `w-8 -brightness-50 saturate-0`
+                }
+              />
+              Share your scheduling link
+            </h2>
+            <p className="w-4/5">
+              Easily book meetings by embedding scheduling links on your
+              website, landing pages, or emails.
+            </p>
+          </div>
+        </div>
+        <div className="py-8 px-4 md:h-[38rem] bg-gray-100 w-full rounded-2xl">
+          {images.map((e, i) => (
+            <img src={e.src} alt="" loading="lazy" key={i} />
+          ))}
+        </div>
+      </>
+    );
+  };
+
   return (
     <section className="mx-auto w-full max-w-7xl my-20">
       <div className="mx-3 md:mx-16">
@@ -11,7 +306,7 @@ const AutoChangeContent = () => {
             className="uppercase font-extrabold text-[.85rem] md:text-[.9rem]"
             style={{ color: 'var(--colors-pink)' }}
           >
-            watch our content
+            watch our contents
           </h1>
           <h2
             className={`font-extrabold text-[1.55rem] text-center w-full mb-4 leading-8 lg:text-start md:text-[2.1rem] md:w-4/5 lg:text-[2.5rem] lg:leading-13`}
@@ -23,6 +318,11 @@ const AutoChangeContent = () => {
             meet the needs of enterprise organizations — including 86% of the
             Fortune 500 companies.
           </p>
+        </div>
+
+        {/* cards */}
+        <div className="grid  grid-cols-1 place-items-center md:grid-cols-2">
+          <ChangingCards />
         </div>
       </div>
     </section>
