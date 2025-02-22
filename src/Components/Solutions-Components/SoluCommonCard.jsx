f472a6f196fa880f0c309bef04a3f595cf83e292
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-22 14:15:20 +0600

    updated the solution why choose page and added more content on the section

--- src/Components/Solutions-Components/SoluCommonCard.jsx
+++ src/Components/Solutions-Components/SoluCommonCard.jsx
@@ -2,29 +2,36 @@ import { FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const SoluCommonCard = ({
+  smallTitle = 'Improve communication rates',
+  bigTitle = 'Book prospects when they’re ready to talk to sales',
+  para = `Remove the steps that cause friction for prospects. Add Calendly to your website, landing pages, or display ads, and embed sales reps’ Calendly links into emails and campaigns to set your team up to win more deals.`,
+  linkText = 'Get DesignPro work anywhere',
   imgSrc = '/images/solutionPage/graphic-design/outbound.png',
+  bgColor,
+  contentOrder = 'order-1',
+  imgOrder = 'order-2',
 }) => {
   return (
-    <section className="w-full max-w-7xl mx-auto">
-      <div className="grid mx-3 md:mx-6 py-8 place-items-center md:grid-cols-2 ">
-        <div className="p-2 grid gap-7 md:w-[93%]">
-          <p className="capitalize text-sm text-blue-800 bg-blue-100 px-2 py-1 w-fit rounded-full">
-            improve communication rates
+    <section className="w-full max-w-7xl mx-auto ">
+      <div
+        className={`grid px-3 py-8 place-items-center md:grid-cols-2 md:px-6 ${bgColor}`}
+      >
+        <div className={`p-2 grid gap-7 md:w-[93%] ${contentOrder}`}>
+          <p className="text-sm text-blue-800 bg-blue-100 px-2 py-1 w-fit rounded-full">
+            {smallTitle}
           </p>
-          <h1 className="text-2xl md:text-4xl">
-            Book prospects when they’re ready to talk to sales
-          </h1>
-          <p className="text-lg text-gray-600">
-            Remove the steps that cause friction for prospects. Add Calendly to
-            your website, landing pages, or display ads, and embed sales reps’
-            Calendly links into emails and campaigns to set your team up to win
-            more deals.
-          </p>
+          <h1 className="text-2xl md:text-4xl">{bigTitle}</h1>
+          <p className="text-lg text-gray-600">{para}</p>
           <Link className="transition-all duration-300 text-blue-500 flex items-center gap-2 hover:gap-4 hover:text-black">
-            Embed Calendly anywhere <FaArrowRight />
+            {linkText} <FaArrowRight />
           </Link>
         </div>
-        <img src={imgSrc} alt="solution common cards" loading="lazy" />
+        <img
+          src={imgSrc}
+          alt="solution common cards"
+          loading="lazy"
+          className={`${imgOrder}`}
+        />
       </div>
     </section>
   );
