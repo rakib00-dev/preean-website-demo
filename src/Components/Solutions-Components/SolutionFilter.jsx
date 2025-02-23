b25606ad48902e848b353791060936061ef1d096
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-23 13:10:40 +0600

    added new section called solution filter

--- src/Components/Solutions-Components/SolutionFilter.jsx
+++ src/Components/Solutions-Components/SolutionFilter.jsx
@@ -1,7 +1,9 @@
+import { NavLink } from 'react-router-dom';
+
 const SolutionFilter = () => {
   return (
     <section className="w-full max-w-7xl md:my-20">
-      <div className="relative py-10">
+      <div className="relative py-10 md:py-30">
         <div className="grid place-items-center mx-auto gap-5">
           <p className="text-sm  text-blue-800 bg-blue-100 px-2 py-1 rounded-full">
             Easy work process
@@ -12,7 +14,39 @@ const SolutionFilter = () => {
             DesignPro's features for clients
           </h2>
         </div>
-        <div className="bg-gray-100 w-full"></div>
+        <div className="flex mt-12 mx-16 w-full text-2xl font-medium">
+          <NavLink
+            className="cursor-pointer py-3 px-6 border-b-3 border-gray-200"
+            style={{ color: '#0b3558' }}
+          >
+            Build Website
+          </NavLink>
+          <NavLink
+            className="cursor-pointer py-3 px-6 border-b-3 border-gray-200"
+            style={{ color: '#0b3558' }}
+          >
+            Thumbnail Design
+          </NavLink>
+          <NavLink
+            className="cursor-pointer py-3 px-6 border-b-3 border-gray-200"
+            style={{ color: '#0b3558' }}
+          >
+            Lead Generation
+          </NavLink>
+          <NavLink
+            className="cursor-pointer py-3 px-6 border-b-3 border-gray-200"
+            style={{ color: '#0b3558' }}
+          >
+            Social Media
+          </NavLink>
+          <NavLink
+            className="cursor-pointer py-3 px-6 border-b-3 border-gray-200"
+            style={{ color: '#0b3558' }}
+          >
+            Canva Design
+          </NavLink>
+        </div>
+        <div className=" w-full"></div>
       </div>
     </section>
   );
