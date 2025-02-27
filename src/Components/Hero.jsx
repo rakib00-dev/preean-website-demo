faa9a9d4588b62db668325d99efa3b863d26c43c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-27 14:43:38 +0600

    added the send request img animation

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Button from './Button';
+import DumbbellShape from './DumbbellShape';
+import TriangleShape from './TriangleShape';
 
 const Hero = ({
   title = 'On-Demand Graphic & Web Design',
@@ -106,33 +108,3 @@ const Hero = ({
 };
 
 export default Hero;
-
-const DumbbellShape = ({ className, fill = '#0099ff' }) => {
-  return (
-    <svg
-      id="Layer_1"
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 547 547"
-      className={className}
-      fill={fill}
-    >
-      <path d="M364.67,164.38h0c-.61,0-1.22.04-1.83.05C350.38,71.51,274.33,0,182.33,0h0C81.63,0,0,85.65,0,191.31h0c0,105.66,81.63,191.31,182.33,191.31h0c.61,0,1.22-.04,1.83-.05,12.46,92.93,88.5,164.43,180.51,164.43h0c100.7,0,182.33-85.65,182.33-191.31h0c0-105.66-81.63-191.31-182.33-191.31Z" />
-    </svg>
-  );
-};
-
-const TriangleShape = ({ className, fill = '#e55cff' }) => {
-  return (
-    <svg
-      id="Layer_1"
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 404.79 144.15"
-      className={className}
-      fill={fill}
-    >
-      <path d="m171.68,7.91L12.27,95.03c-20.27,11.08-13.96,49.18,8.14,49.12l364.05-.85c22.08-.05,28.29-38.16,8-49.16L233.1,7.79c-19.25-10.43-42.21-10.39-61.43.11Z" />
-    </svg>
-  );
-};
--- src/Components/SendReqSection.jsx
+++ src/Components/SendReqSection.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import { motion } from 'framer-motion';
+import DumbbellShape from './DumbbellShape';
+import TriangleShape from './TriangleShape';
 
 const SendReqSection = ({ title, button }) => {
   return (
@@ -14,8 +16,20 @@ const SendReqSection = ({ title, button }) => {
       }}
       className="mx-auto my-15 w-full max-w-7xl"
     >
-      <div className="mx-5">
-        <div className="w-full relative text-center py-6 rounded-md flex-col gap-10 leading-10 bg-yellow-400 h-auto justify-center items-center md:flex md:rounded-xl xl:rounded-2xl">
+      <div className="relative mx-5 md:mx-0 overflow-hidden">
+        <div className="top-10 -right-30 absolute z-10 hidden md:block">
+          <TriangleShape
+            className={
+              'absolute top-0 right-0 w-[26rem] opacity-100 SendRedTriangleShapeAnima rotate-90 '
+            }
+          />
+          <DumbbellShape
+            className={
+              'absolute top-0 right-0 w-[20rem] opacity-80 rotate-90 SendRedDumbbellShapeAnima '
+            }
+          />
+        </div>
+        <div className="w-full relative text-center py-6 rounded-md flex-col gap-10 leading-10 bg-[#f0f3f8] h-auto justify-center items-center md:flex ">
           {title ? (
             title
           ) : (
@@ -31,18 +45,20 @@ const SendReqSection = ({ title, button }) => {
           {button ? (
             button
           ) : (
-            <div className="flex flex-col mb-15 gap-5 md:mb-0 md:flex-row">
-              <Link to="/contact">
-                <Button
-                  text={'Get Started'}
-                  className={'text-white hover:bg-white hover:text-black'}
-                />
+            <div className="flex items-center flex-col mb-15 gap-5 md:mb-0 md:flex-row">
+              <Link
+                to="/contact"
+                className={
+                  'transition-all duration-300 font-bold bg-blue-500 px-4 py-[1px] rounded text-white hover:bg-white hover:text-black hover:shadow-lg '
+                }
+              >
+                Get Started
               </Link>
               <Link to="/book-a-call">
                 <Button
                   text={'Book A Call'}
                   className={
-                    'border-gray-700 border-2 hover:border-blue-500 hover:text-blue-500'
+                    'transition-all duration-300 border-gray-700 border-2 hover:border-blue-500 hover:text-blue-500 hover:shadow-lg'
                   }
                   bg="transparent"
                 />
@@ -52,6 +68,18 @@ const SendReqSection = ({ title, button }) => {
 
           {/* <div className="bg-pink-500 rounded-bl-lg rounded-br-lg w-15 h-10 absolute inset-[0%_6.375rem_auto_auto] right-5 z-12 md:w-20 md:h-16 md:right-10"></div> */}
           {/* <div className="bg-[#0a32c3] rounded-tl-lg rounded-tr-lg w-15 h-10 absolute bottom-0 left-5 z-12 md:w-20 md:h-16 md:left-10"></div> */}
+        </div>
+        <div className="top-10 -left-30 absolute hidden md:block">
+          <TriangleShape
+            className={
+              'absolute top-0 w-[26rem] opacity-100 SendRedTriangleShapeAnima rotate-270 hidden md:block'
+            }
+          />
+          <DumbbellShape
+            className={
+              'absolute top-0 w-[20rem] opacity-80 SendRedDumbbellShapeAnima hidden md:block'
+            }
+          />
         </div>
       </div>
     </motion.section>
