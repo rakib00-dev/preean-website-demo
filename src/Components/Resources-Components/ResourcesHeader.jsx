93a1e0087a8cb4cc81afe2f63be9f006ce685885
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-27 15:55:42 +0600

    update the resources header bg and img

--- src/Components/Resources-Components/ResourcesHeader.jsx
+++ src/Components/Resources-Components/ResourcesHeader.jsx
@@ -1,7 +1,25 @@
+import DumbbellShape from '../DumbbellShape';
+import RoundedStar from '../RoundedStar';
+import SemiCircle from '../SemiCircle';
+import TriangleShape from '../TriangleShape';
+
 const ResourcesHeader = () => {
   return (
-    <section className="w-full max-w-7xl md:my-4 overflow-hidden">
-      <div className="relative py-10">
+    <section className="w-full max-w-7xl overflow-hidden">
+      <div className="relative py-10 bg-[#f0f3f8]">
+        <div className="top-10 -right-30 absolute z-10 hidden md:block">
+          <RoundedStar
+            className={
+              'absolute top-0 right-0 w-[20rem] opacity-100 SendReqRoundedStarAnima rotate-90 '
+            }
+            fill="#ef457e"
+          />
+          <SemiCircle
+            className={
+              'absolute top-0 right-0 w-[20rem] opacity-80 rotate-90 SendReqSemiCircleAnima '
+            }
+          />
+        </div>
         <div className="grid place-items-center mx-auto gap-5">
           <p className="text-sm  text-blue-800 bg-blue-100 px-2 py-1 rounded-full">
             Our work process
@@ -11,6 +29,18 @@ const ResourcesHeader = () => {
           >
             Take a look at DesignPro's work process for clients
           </h2>
+        </div>
+        <div className="top-10 -left-30 absolute hidden md:block">
+          <TriangleShape
+            className={
+              'absolute top-0 w-[26rem] opacity-100 SendRedTriangleShapeAnima rotate-270 hidden md:block'
+            }
+          />
+          <DumbbellShape
+            className={
+              'absolute top-0 w-[20rem] opacity-80 SendRedDumbbellShapeAnima hidden md:block'
+            }
+          />
         </div>
       </div>
     </section>
