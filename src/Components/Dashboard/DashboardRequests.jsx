6635e8986fa4326dcdeae85538853e753b041bed
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-05 12:44:01 +0600

    added some content at dashboard request

--- src/Components/Dashboard/DashboardRequests.jsx
+++ src/Components/Dashboard/DashboardRequests.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { GoPlus } from 'react-icons/go';
+import { IoMdArrowDropright } from 'react-icons/io';
 
 const DashboardRequests = () => {
   return (
-    <section className="mx-10">
-      <h1 className="text-6xl">Dashboard Requests</h1>
+    <section className="mx-50">
+      <div className="grid gap-2">
+        <div className="flex justify-between items-center">
+          <h1 className="text-4xl">Requests</h1>
+          <button className="flex text-lg bg-blue-500 font-medium items-center justify-center text-white py-1 px-2 gap-1 transition-all duration-300 hover:bg-blue-400 cursor-pointer">
+            <GoPlus className="text-xl" /> Create Requests
+          </button>
+        </div>
+        <p
+          className="w-96 mb-7"
+          style={{ color: 'var(--black-primary-terms)' }}
+        >
+          Store all your requests assets in one place. This will help you save
+          time when submitting new requests.
+        </p>
+        <div>
+          <h3 className="text-xl flex justify-start items-end">
+            requests
+            <IoMdArrowDropright className="transition-all duration-300 text-2xl rotate-90 text-blue-500" />
+          </h3>
+        </div>
+      </div>
     </section>
   );
 };
