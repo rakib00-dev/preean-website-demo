7b856e34f0087e49d722e5ca26261d8b71002ff1
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-05 13:09:25 +0600

    added search button

--- src/Components/Dashboard/DashboardRequests.jsx
+++ src/Components/Dashboard/DashboardRequests.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoPlus } from 'react-icons/go';
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
 import { IoMdArrowDropright } from 'react-icons/io';
 
 const DashboardRequests = () => {
@@ -8,9 +9,15 @@ const DashboardRequests = () => {
       <div className="grid gap-2">
         <div className="flex justify-between items-center">
           <h1 className="text-4xl">Requests</h1>
-          <button className="flex text-lg bg-blue-500 font-medium items-center justify-center text-white py-1 px-2 gap-1 transition-all duration-300 hover:bg-blue-400 cursor-pointer">
-            <GoPlus className="text-xl" /> Create Request
-          </button>
+          <div className="flex items-center gap-2">
+            <div className="flex justify-center items-center bg-gray-100 shadow-md border px-4 border-gray-200 py-1.5">
+              <HiOutlineMagnifyingGlass />
+              <input type="search" />
+            </div>
+            <button className="flex text-lg bg-blue-500 font-medium items-center justify-center text-white py-1 px-2 gap-1 transition-all duration-300 hover:bg-blue-400 cursor-pointer">
+              <GoPlus className="text-xl" /> Create Request
+            </button>
+          </div>
         </div>
         <p
           className="w-96 mb-7"
