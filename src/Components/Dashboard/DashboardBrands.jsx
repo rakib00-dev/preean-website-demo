e299c308430eee93b86c48f82b6f4b5c306f40f0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-05 16:08:37 +0600

    added dashboardBrands cards

--- src/Components/Dashboard/DashboardBrands.jsx
+++ src/Components/Dashboard/DashboardBrands.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { BsThreeDots } from 'react-icons/bs';
 import { GoPlus } from 'react-icons/go';
 import { IoMdArrowDropright } from 'react-icons/io';
 
 const DashboardBrands = () => {
+  const brandArr = [
+    {
+      src: 'https://yt3.googleusercontent.com/OqN3DP0rwXrSBvzuAgfeEGwkdhheV93iWz5KsyMeCgk11AJ9B_F2FvIDTN06cNmQ310twIsU5xU=s160-c-k-c0x00ffffff-no-rj',
+      title: 'ManyPixels',
+      usedInReq: 29,
+    },
+    {
+      src: 'https://yt3.googleusercontent.com/OqN3DP0rwXrSBvzuAgfeEGwkdhheV93iWz5KsyMeCgk11AJ9B_F2FvIDTN06cNmQ310twIsU5xU=s160-c-k-c0x00ffffff-no-rj',
+      title: 'ManyPixels',
+      usedInReq: 63,
+    },
+    {
+      src: 'https://yt3.googleusercontent.com/OqN3DP0rwXrSBvzuAgfeEGwkdhheV93iWz5KsyMeCgk11AJ9B_F2FvIDTN06cNmQ310twIsU5xU=s160-c-k-c0x00ffffff-no-rj',
+      title: 'ManyPixels',
+      usedInReq: 11,
+    },
+    {
+      src: 'https://yt3.googleusercontent.com/OqN3DP0rwXrSBvzuAgfeEGwkdhheV93iWz5KsyMeCgk11AJ9B_F2FvIDTN06cNmQ310twIsU5xU=s160-c-k-c0x00ffffff-no-rj',
+      title: 'ManyPixels',
+      usedInReq: 11,
+    },
+  ];
+
+  const [brand, setBrand] = useState([...brandArr]);
+
   return (
     <section className="mx-50">
       <div className="grid gap-2">
@@ -24,9 +51,38 @@ const DashboardBrands = () => {
             <IoMdArrowDropright className="transition-all duration-300 text-2xl rotate-90 text-blue-500" />
           </h3>
         </div>
+        <div className="grid gap-3 grid-cols-3">
+          {brand.map((e, i) => (
+            <Cards
+              key={i}
+              title={e.title}
+              usedInReq={e.usedInReq}
+              src={e.src}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
 };
 
 export default DashboardBrands;
+
+const Cards = ({ src, title, usedInReq }) => {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-2 py-2 px-4 border border-gray-200 rounded-xl bg-gray-100">
+        <div className="flex gap-2 justify-start">
+          <img src={src} alt="" loading="lazy" className="w-12 rounded-full" />
+          <div>
+            <h3>{title}</h3>
+            <p>used in {usedInReq} requests</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center py-4 px-2 cursor-pointer">
+          <BsThreeDots />
+        </div>
+      </div>
+    </>
+  );
+};
