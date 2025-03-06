8187f0aeb5d6fc67f3599feb53aab7b6ce00bf8d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-06 15:40:02 +0600

    fixed the uuppercase error

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import DashboardNavbar from '../Components/Dashboard/dashboardNavbar';
+import DashboardNavbar from '../Components/Dashboard/DashboardNavbar';
 
 const DashboradLayout = () => {
   return (
